ab747bb7400d6a8927d4ccf70d024738d68bb563
Sasha Shamne <alexander.shamne@gmail.com>
2015-03-18 15:07:19 -0700

    Add header search form functionality

--- assets/js/header/controller.js
+++ assets/js/header/controller.js
@@ -1,24 +1,36 @@
 'use strict';
 
- function HeaderCtrl($scope, $rootScope, $location, $timeout,  $http, CurrentUser) {
+function HeaderCtrl($scope, $rootScope, $location, $timeout,  $http, CurrentUser) {
 
-   /*
-    * User Sign-in Function
-    */
-    $scope.signOut = function() {
-      var cb = function() {
-        $rootScope.isSignedIn = false;
-        $('#logout-modal').foundation('reveal','open');
-        CurrentUser.clear();
-      };
-      $http.delete('/login', {
-        params: {
-          _csrf: SAILS_LOCALS._csrf
-        },
-      }).success(cb).error(cb);
+  /*
+   * Sign Out click handler
+   *
+   */
+  $scope.signOut = function() {
+    var cb = function() {
+      $rootScope.isSignedIn = false;
+      $('#logout-modal').foundation('reveal','open');
+      CurrentUser.clear();
+    };
+    $http.delete('/login', {
+      params: {
+        _csrf: SAILS_LOCALS._csrf
+      },
+    }).success(cb).error(cb);
+  };
+
+  /*
+   * Header Search Form Handler
+   * Sends to a Discover page with a search query
+   *
+   */
+  $scope.headerSearch = function() {
+    if ($scope.headerSearchQuery) {
+      $location.search('q', $scope.headerSearchQuery).path('/discover');
     }
- }
+    return false;
+  };
+}
 
-
- HeaderCtrl.$inject = ['$scope', '$rootScope', '$location', '$timeout', '$http', 'CurrentUser'];
- myStandControllers.controller('HeaderCtrl', HeaderCtrl);
+HeaderCtrl.$inject = ['$scope', '$rootScope', '$location', '$timeout', '$http', 'CurrentUser'];
+myStandControllers.controller('HeaderCtrl', HeaderCtrl);
