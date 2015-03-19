fd7d0bcff372670f3fc48b8dab50431d141c66c1
mattwilliams85 <mattwilliams85@gmail.com>
2015-03-19 14:42:54 -0700

    update sign up page; can now create account

--- assets/js/sign-up/controller.js
+++ assets/js/sign-up/controller.js
@@ -1,25 +1,34 @@
 'use strict';
 
-function SignUpCtrl($scope, $rootScope, $http) {
-  $scope.user = {};
-  debugger
-  $scope.signUp = function() {
+function SignUpCtrl($scope, $rootScope, $location, $timeout,  $http) {
+  $scope.newUser = {}
 
-  }
+  $scope.userSignUp = function() {
+    if($scope.newUser.email && $scope.newUser.password) {
+      $http.post('/users.json', {
+        _csrf: SAILS_LOCALS._csrf,
+        email: $scope.newUser.email,
+        password: $scope.newUser.password
+      }).
+      success(function(data) {
+        if (data.error) {
+          console.log('invalid username or password')
+            // $scope.showModal('Oops, wrong email or password');
+            // $(document).foundation();
+        } else {
+          console.log('newUser created!')
+          $location.path('discover')
+        }
+      }).
+      error(function() {
+        console.log('Log In Error');
+      });
+    } else {
+      console.log('fields missing')
+    }
+  };
 
-  // this.init($scope);
-  // this.fetch($scope);
 }
 
-// SignUpCtrl.prototype.init = function($scope) {
-//   // Setting mode based on the url
-//   $scope.mode = 'index';
-// };
-
-// SignUpCtrl.prototype.fetch = function($scope) {
-//   if ($scope.mode === 'index') {
-//   }
-// };
-
-SignUpCtrl.$inject = ['$scope', '$rootScope', '$http'];
+SignUpCtrl.$inject = ['$scope', '$rootScope', '$location', '$timeout', '$http'];
 myStandControllers.controller('SignUpCtrl', SignUpCtrl);
