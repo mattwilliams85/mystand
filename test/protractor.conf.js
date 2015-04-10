13e6e7280f141b19193105e83699d11ecd6fc119
Sasha Shamne <alexander.shamne@gmail.com>
2015-04-10 11:29:13 -0700

    Update protractor config part 2

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -2,7 +2,6 @@
 
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  // seleniumAddress: 'http://localhost:4444/wd/hub',
   // capabilities: {
   //   'browserName': 'chrome'
   // },
