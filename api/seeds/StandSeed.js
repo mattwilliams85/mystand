73a5107b6de48f3af8400500553fdecde487b7dd
Sasha Shamne <alexander.shamne@gmail.com>
2015-04-20 10:26:43 -0700

    Fix seeder

--- api/seeds/StandSeed.js
+++ api/seeds/StandSeed.js
@@ -12,17 +12,17 @@ module.exports = {
     var youtubeVideos = ['kWTQZYMIEKE', 'kdrYtE2Us1g', 'JtJgbd1Jfuk', '3HAuTQGVgjc', 'CvEoncP-2Mc', '5iTTNRE-njM', 'y-JWXF-Pud4', 'kxopViU98Xo', 'ZmEs6YhCEO8'];
     var seedData = [];
     var image;
-    var coinFlip;
+    // var coinFlip;
     for (var i = 0; i < 30; i++) {
       image = 'http://lorempixel.com/640/480/nature/' + (Math.floor(Math.random() * 10) + 1);
-      coinFlip = Math.round(Math.random() * 1)
+      // coinFlip = Math.round(Math.random() * 1);
 
       seedData.push({
         user: Math.floor(Math.random() * 3) + 1,
         category: Math.floor(Math.random() * 9) + 1,
         title: chance.word() + ' ' + chance.word(),
-        image_original_url: coinFlip ? image : null,
-        youtube: !coinFlip ? youtubeVideos[Math.floor(Math.random() * youtubeVideos.length) + 1] : null,
+        image_original_url: image,
+        youtube: youtubeVideos[Math.floor(Math.random() * youtubeVideos.length) + 1],
         description: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec id elit non mi porta gravida at eget metus!',
         goal_result: 'We\'ll do this and that ...',
         goal: 100,
